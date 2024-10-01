629765c99c1bdf1fcf38e02a5f7649c5cf45a258
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2024-10-01 17:31:17 -0300

    Implemented update pet properties endpoint

---
+++ GatilDosResgatadosApi/Areas/Pets/UpdatePet.cs
@@ -0,0 +1,74 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using GatilDosResgatadosApi.Areas.Pets.Entities;
+using GatilDosResgatadosApi.Infrastructure.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace GatilDosResgatadosApi.Areas.Pets;
+
+public record UpdatePetRequest
+{
+    public string Id { get; set; } = default!;
+    public string? Name { get; set; }
+    public double? Weight { get; set; }
+    public string? Description { get; set; }
+    public Gender? Gender { get; set; }
+    public IFormFile? Avatar { get; set; }
+}
+
+public class UpdatePetRequestValidator : Validator<UpdatePetRequest>
+{
+    public UpdatePetRequestValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("O código do pet é obrigatório");
+    }
+}
+
+public class UpdatePet(ApplicationDbContext dbContext, ILogger<UpdatePetRequest> logger) : Endpoint<UpdatePetRequest, Results<NoContent, NotFound, ProblemHttpResult>>
+{
+    public override void Configure()
+    {
+        Patch("api/pet/");
+        AllowFileUploads();
+    }
+
+    public async override Task<Results<NoContent, NotFound, ProblemHttpResult>> ExecuteAsync(UpdatePetRequest req, CancellationToken ct)
+    {
+        var pet = await dbContext.Pets.FirstOrDefaultAsync(x => x.Id == req.Id, ct);
+        if (pet is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        if (!string.IsNullOrWhiteSpace(req.Name))
+            pet.Name = req.Name;
+        if (req.Weight is not null)
+            pet.Weight = req.Weight;
+        if (!string.IsNullOrWhiteSpace(req.Description))
+            pet.Description = req.Description;
+        if (req.Gender is not null)
+            pet.Gender = req.Gender;
+
+        if (req.Avatar?.Length > 0)
+        {
+            using var memoryStream = new MemoryStream();
+            await req.Avatar.CopyToAsync(memoryStream, ct);
+            pet.Avatar = memoryStream.ToArray();
+        }
+
+        try
+        {
+            dbContext.Update(pet);
+            await dbContext.SaveChangesAsync(ct);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Could not update pet {pet}. Error: {error}", pet, ex);
+            return TypedResults.Problem("Could not save to database");
+        }
+        return TypedResults.NoContent();
+    }
+}
