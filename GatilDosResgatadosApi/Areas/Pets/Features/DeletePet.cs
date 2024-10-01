765379e3885904b443625eab93571770b7d8b5cf
Kevin Kouketsu <kevinkouketsu53@gmail.com>
2024-10-01 17:56:16 -0300

    Implemented delete pet endpoint

---
+++ GatilDosResgatadosApi/Areas/Pets/Features/DeletePet.cs
@@ -0,0 +1,38 @@
+﻿using FastEndpoints;
+using GatilDosResgatadosApi.Infrastructure.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GatilDosResgatadosApi.Areas.Pets.Features;
+
+public record DeletePetRequest([FromRoute] string Id);
+
+public class DeletePet(ApplicationDbContext dbContext, ILogger<DeletePet> logger) : Endpoint<DeletePetRequest, Results<NoContent, NotFound, ProblemHttpResult>>
+{
+    public override void Configure()
+    {
+        Delete("/api/pet/{id}");
+    }
+
+    public async override Task<Results<NoContent, NotFound, ProblemHttpResult>> ExecuteAsync(DeletePetRequest req, CancellationToken ct)
+    {
+        var pet = await dbContext.Pets.FindAsync(req.Id);
+        if (pet is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        try
+        {
+            dbContext.Pets.Remove(pet);
+            await dbContext.SaveChangesAsync(ct);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("Could not delete pet {pet}. Error: {ex}", pet.ToString(), ex);
+            return TypedResults.Problem("Could not save to database");
+        }
+
+        return TypedResults.NoContent();
+    }
+}
